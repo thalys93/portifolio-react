ed5606c8f3814b1509136670c15716780b06cc86
Luis T. Rodrigues <luisthalys1@hotmail.com>
2023-03-14 16:43:11 -0300

    Criação do Projeto
    - Criação do Projeto
    - Utilizando o Vite (para o react)
    - Criação do .bat (para inicialização do projeto)
    - Atualizado Algumas Informações do Index.html

---
+++ README.md
@@ -0,0 +1,2 @@
+# portifolio-react
+ Portifolio criado usando tecnologias como React e Strapi(para banco de dados)
---
+++ src/main.jsx
@@ -0,0 +1,14 @@
+// Imports do React e React DOM
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+
+// Import de Estilos Globais
+import './global.css'
+
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    
+  </React.StrictMode>,
+)
