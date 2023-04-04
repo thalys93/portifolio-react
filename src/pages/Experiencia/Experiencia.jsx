e270ac3bb1737b0522e5f9c526d005117f233f8e
Luis T. Rodrigues <luisthalys1@hotmail.com>
2023-04-04 18:46:14 -0300

    Pegando os Dados da Branch Not
    Pegando os dados da branch e passando para o PC, para desenvolvimento.

--- src/pages/Experiencia/Experiencia.jsx
+++ src/pages/Experiencia/Experiencia.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { FaBookmark } from 'react-icons/fa'
 
 // CSS
 import './exp.css'
@@ -8,15 +9,16 @@ function Experiencia() {
     <section>      
       <ol id='expList'>
         <li id='expObj'>          
+            <FaBookmark id='expIcon'/>
             <div id='expTitle'>
           <h3> Experiência (Exemplo) </h3> 
           <h3> 2022 - 2023 </h3>
-            </div>
+            </div>          
             <div id='expDetails'>
-          <h5> Estagiário </h5> <p> : </p>
+          <h5> Estagiário </h5>
           <p> Exemplo de Atividades de Estagiário </p>
             </div>
-        </li>
+        </li>        
       </ol>
     </section>
   )
--- src/pages/Projetos/Projetos.jsx
+++ src/pages/Projetos/Projetos.jsx
@@ -1,10 +1,23 @@
 import React from 'react'
 
+import './projects.css'
+
 function Projetos() {
   return (
-    <div>
-      <h1> Projetos </h1>
-    </div>
+    <section>      
+      <ol id='pjList'>
+        <li id='pjCard'>          
+            <div id='pjTitle'>
+          <h3> Projeto (Nome) </h3>           
+            </div>
+            <div id='pjDetails'>
+          <h6> (Categoria do Projeto) </h6>
+          <h5> Thumbnail </h5>        
+          <p> Descrição do Projeto </p>
+            </div>
+        </li>        
+      </ol>
+    </section>
   )
 }
 
